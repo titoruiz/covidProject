4deca632ed86f5d960a9dda2d5fcd47bf308c772
Mahan Mahtabfar <mmahtabfar1@gmail.com>
2021-03-21 14:33:37 -0400

    added react-vis sample chart to MainPage.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import WelcomePage from "./WelcomePage";
 import MainPage from "./MainPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
+  //change the document title from "React App"
+  useEffect(() => {
+    document.title = "CovidProject";
+  }, []);
+
   return (
     <Router>
       <Switch>
--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -1,7 +1,59 @@
 import React from "react";
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  ChartLabel,
+  HorizontalGridLines,
+  VerticalGridLines,
+  LineSeries,
+} from "react-vis";
+import "./App.css";
 
 const MainPage = () => {
-  return <h1>Hello!</h1>;
+  return (
+    <>
+      <div className="App-header">
+        <h1>CovidProject</h1>
+      </div>
+      <div className="CenterScreen">
+        <XYPlot width={600} height={500}>
+          <HorizontalGridLines />
+          <VerticalGridLines />
+          <XAxis />
+          <YAxis />
+          <ChartLabel
+            text="X Axis"
+            className="alt-x-label"
+            includeMargin={false}
+            xPercent={0.9}
+            yPercent={1.01}
+          />
+
+          <ChartLabel
+            text="Y Axis"
+            className="alt-y-label"
+            includeMargin={false}
+            xPercent={0.06}
+            yPercent={0.06}
+            style={{
+              transform: "rotate(-90)",
+              textAnchor: "end",
+            }}
+          />
+          <LineSeries
+            className="first-series"
+            data={[
+              { x: 1, y: 3 },
+              { x: 2, y: 5 },
+              { x: 3, y: 15 },
+              { x: 4, y: 12 },
+            ]}
+          />
+        </XYPlot>
+      </div>
+    </>
+  );
 };
 
 export default MainPage;
