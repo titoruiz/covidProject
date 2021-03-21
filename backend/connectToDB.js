937c39ac4b78d9411f540ceda11ea89d01b79984
Mahan Mahtabfar <mmahtabfar1@gmail.com>
2021-03-20 21:34:43 -0400

    add functionality to connect the backend to CISE oracle.

---
+++ backend/connectToDB.js
@@ -0,0 +1,28 @@
+const oracledb = require('oracledb');
+
+const connectToDb = async (user, pass) => {
+    
+    let connection;
+    console.log("connecting to oracleDB");
+
+    try {
+        connection = await oracledb.getConnection(
+            {
+                "user": user,
+                "password": pass,
+                "connectionString": "oracle.cise.ufl.edu:1521/orcl",
+            }
+        );
+    }
+    catch(err) {
+        console.error(err);
+    }
+    finally {
+        if(connection) return connection;
+        
+        console.error("Unable to connect to Oracle");
+        process.exit(1);
+    }
+}
+
+module.exports = connectToDb;
