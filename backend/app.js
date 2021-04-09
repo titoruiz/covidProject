530f944bcaae435a9a4ab876ac60ce23a66b04c1
Mahan Mahtabfar <mmahtabfar1@gmail.com>
2021-04-09 01:18:49 -0400

    added api routes to make queries to database and made additions to welcomePage.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const oracledb = require("oracledb");
-const connectToDB = require("./connectToDB");
 const secrets = require("./secrets");
+const connectToDB = require("./connectToDB");
 
 //create instance of express and define the port
 const app = express();
@@ -20,11 +20,39 @@ oracledb.initOracleClient({
 //get the connection to oracle
 connectToDB(secrets.user, secrets.password).then((connection) => {
   //express routes
+
+  //serve the static files here from the webpage
   app.get("/", (req, res) => {
     connection.execute(`SELECT * FROM Country`).then((result) => {
       console.log(result.rows);
       res.send("Hello, World!");
     });
+  });
+
+  //api endpoints to retrieve information from the database
+  app.get("/api/one/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send("in endpoint one");
+  });
+
+  app.get("/api/two/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send("in endpoint two");
+  });
+
+  app.get("/api/three/:id", (req, res) => {
+    consol.log(req.params.id);
+    res.send("in endpoint three");
+  });
+
+  app.get("/api/four/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send("in endpoint four");
+  });
+
+  app.get("/api/five/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send("in endpoint five");
   });
 
   //start the express server
--- frontend/src/WelcomePage.js
+++ frontend/src/WelcomePage.js
@@ -10,6 +10,43 @@ const WelcomePage = () => {
           Take Me To The App!
         </Link>
       </header>
+      <div id="AboutSection">
+        <h2 className="AboutTitle">About This Project:</h2>
+        <p>
+          Our team wanted to create a web app that would benefit users Our team
+          wanted to create a web app that would benefit usersOur team wanted to
+          create a web app that would benefit usersOur team wanted to create a
+          web app that would benefit usersOur team wanted to create a web app
+          that would benefit usersOur team wanted to create a web app that would
+          benefit usersOur team wanted to create a web app that would benefit
+          usersOur team wanted to create a web app that would benefit usersOur
+          team wanted to create a web app that would benefit usersOur team
+          wanted to create a web app that would benefit usersOur team wanted to
+          create a web app that would benefit usersOur team wanted to create a
+          web app that would benefit usersOur team wanted to create a web app
+          that would benefit usersOur team wanted to create a web app that would
+          benefit users
+        </p>
+        <h2 className="DatasetsTitle">Datasets Utilized:</h2>
+        <p>
+          usersOur team wanted to create a web app that would benefit usersOur
+          team wanted to create a web app that would benefit usersOur team
+          wanted to create a web app that would benefit usersOur team wanted to
+          create a web app that would benefit usersOur team wanted to create a
+          web app that would benefit usersOur team wanted to create a web app
+        </p>
+        <h2 className="LinksTitle">Links to public Datasets:</h2>
+        <a href="https://covidtracking.com/data/api">
+          Covid Tracking Project by The Atlantic
+        </a>
+        <a href="https://openweathermap.org/api">Open Weather Map</a>
+        <a href="https://www.acaps.org/covid-19-government-measures-dataset">
+          COVID-19 Government Measures Dataset
+        </a>
+        <a href="https://www.worldpop.org/sdi/introapi">
+          World Population Dataset
+        </a>
+      </div>
     </div>
   );
 };
