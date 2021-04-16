bdefc1c59d9a9a2bc86028845a26cb776129690a
titoruiz <57964382+titoruiz@users.noreply.github.com>
2021-04-16 14:44:59 -0400

    Update Welcome Page
    Description and Updated Links

--- frontend/src/WelcomePage.js
+++ frontend/src/WelcomePage.js
@@ -9,29 +9,39 @@ const WelcomePage = () => {
         <p className="Title">Covid-Project</p>
       </header>
       <div id="AboutSection">
+        <h2 className="DatasetsTitle">Collaborators</h2>
+        <p>Greg Bouraoui</p>
+        <p>Mahan Mahtabfar</p>
+        <p>Nathan Fox</p>
+        <p>Tito Ruiz</p><br/>
         <h2 className="AboutTitle">About This Project:</h2>
         <p>
-          The main purpose of this project is to identify trends with regards to season, temperature, elevation,
-          and COVID restriction measures. Most of our queries involve comparing data and seeing if there is a 
-          a difference in COVID cases or deaths between the queries. Being able to identify these trends and associations 
-          can inform us about the factors that allow this virus to spread. Knowing these factors will allow people to 
-          make informed decisions in regards to their health.
+        Our team put together a database application that utilizes COVID-19 information 
+        to analyze trends of the disease over time and get a better grasp of how it’s being 
+        spread. With this information mainly based at the country, state, and county levels, 
+        the users will be able to sort through our organized data to observe the different 
+        trends over time based on different conditions. These trends that are going to be shown
+        are meant to provide insight on specific information we have selected that we believe to 
+        have the biggest impact on the spreading and contraction of the virus due to their factors. 
+        The main focus was to leave the users who visit our site better informed on the spreading 
+        and development of the coronavirus; applying their interpretations to their own situations 
+        in order to live a healthier and safer life during these hard times.
         </p>
         <h2 className="DatasetsTitle">Datasets Utilized:</h2>
         <p>
           Most of our data was collected from several dataset which had the corresponding information
-          needed for the trends we wanted to use. 
+          needed for each trend. The data was normalized so it wasn't redundant and was able to fit 
+          into the schemas made for them in Oracle. 
         </p>
         <h2 className="LinksTitle">Links to public Datasets:</h2>
-        <a href="https://covidtracking.com/data/api">
-          Covid Tracking Project by The Atlantic
-        </a>
-        <a href="https://openweathermap.org/api">Open Weather Map</a>
         <a href="https://www.acaps.org/covid-19-government-measures-dataset">
           COVID-19 Government Measures Dataset
         </a>
-        <a href="https://www.worldpop.org/sdi/introapi">
-          World Population Dataset
+        <a href="https://ourworldindata.org/">
+          Country COVID Dataset
+        </a>
+        <a href="https://covidtracking.com/data/api">
+          State COVID Dataset
         </a>
         <a href="https://www.worldpop.org/sdi/introapi">
           County COVID Dataset
